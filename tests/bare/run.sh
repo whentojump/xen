d63aa6cf8e6b516c36efd6c3fa279dc55143a5cf
Wentao Zhang <zhangwt1997@gmail.com>
2025-11-28 17:59:24 -0600

    user test

--- tests/bare/run.sh
+++ tests/bare/run.sh
@@ -3,7 +3,7 @@ export PATH=/home/wentaoz5/wentaoz5/ktest-project/xen-patches/llvm-bare-install/
 rm -rf test *.prof*
 
 # -u__llvm_profile_runtime /media/wd-sn580-2t-1/users/wentaoz5/ktest-project/xen-patches/llvm-bare-install/lib/clang/22/lib/x86_64-unknown-linux-gnu/libclang_rt.profile.a
-clang -v -c -fprofile-instr-generate -fcoverage-mapping -fcoverage-mcdc -o test.o test.c
+clang `#-v` -c -fprofile-instr-generate -fcoverage-mapping -fcoverage-mcdc -o test.o test.c
 clang -fprofile-instr-generate -fcoverage-mapping -fcoverage-mcdc -o test test.o
 ./test
 
@@ -13,3 +13,8 @@ llvm-nm test   | grep -E '(__llvm_profile_runtime|__llvm_profile_get_size_for_bu
 echo ----
 llvm-nm /media/wd-sn580-2t-1/users/wentaoz5/ktest-project/xen-patches/llvm-bare-install/lib/clang/22/lib/x86_64-unknown-linux-gnu/libclang_rt.profile.a |\
     grep -E '(__llvm_profile_runtime|__llvm_profile_get_size_for_buffer|__llvm_profile_write_buffer)'
+
+if [[ -f manual.profraw ]]; then
+    llvm-profdata merge manual.profraw -o manual.profdata
+    llvm-cov show test -instr-profile=manual.profdata -show-branches=count -show-mcdc
+fi
--- tests/bare/test.c
+++ tests/bare/test.c
@@ -1,9 +1,26 @@
-// void dump() {
-//     __llvm_profile_get_size_for_buffer();
-// }
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+uint64_t __llvm_profile_get_size_for_buffer(void);
+int __llvm_profile_write_buffer(char *Buffer);
+
+void dump() {
+    FILE *fd = fopen("manual.profraw", "w");
+    uint64_t size = __llvm_profile_get_size_for_buffer();
+    char *buffer = (char *)malloc(size);
+    __llvm_profile_write_buffer(buffer);
+    fwrite(buffer, 1, size, fd);
+    fclose(fd);
+    free(buffer);
+}
 
 int main() {
     int a = 1, b = 2;
-    if ( a && b ) return 3;
+    if ( a && b ) {
+        dump();
+        return 3;
+    }
+    dump();
     return 4;
 }
